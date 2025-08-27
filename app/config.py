c479ccfeb6f92bfe605eafa1b63c70489646c067
Haris <harissabil04@gmail.com>
2025-08-27 14:43:38 +0700

    Add DATA_SERVICE_BASE_URL configuration to settings

--- app/config.py
+++ app/config.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     azure_openai_deployment: str = Field(alias="AZURE_OPENAI_DEPLOYMENT")
     azure_openai_api_version: str = Field(alias="AZURE_OPENAI_API_VERSION")
 
+    data_service_base_url: AnyUrl = Field(alias="DATA_SERVICE_BASE_URL")
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
