39d6356c5ec4ab52f6e95ab45c47c70415a44d8e
Haris <harissabil04@gmail.com>
2025-09-08 10:22:18 +0700

    Add Google Search Console tools to the main import list

--- app/agent/tools/__init__.py
+++ app/agent/tools/__init__.py
@@ -6,6 +6,14 @@ from .google_analytics import (
     get_google_analytics_traffic_by_countries,
     get_google_analytics_traffic_by_pages,
 )
+from .google_search_console import (
+    get_search_console_countries,
+    get_search_console_daily,
+    get_search_console_daily_for_country,
+    get_search_console_daily_for_keyword,
+    get_search_console_keywords,
+    get_search_console_overall,
+)
 from .utils import get_current_datetime
 
 # --- When you add more tools, just import them and add them to this list ---
@@ -25,7 +33,14 @@ google_analytics_tools = [
     get_google_analytics_daily_traffic_for_page,
 ]
 
-# search_console_tools = [get_search_console_clicks, ...]
+search_console_tools = [
+    get_search_console_overall,
+    get_search_console_daily,
+    get_search_console_countries,
+    get_search_console_daily_for_country,
+    get_search_console_keywords,
+    get_search_console_daily_for_keyword,
+]
 
 # Combine all tools into one list for the agent
-all_tools = utility_tools + google_analytics_tools  # + search_console_tools + admob_tools
+all_tools = utility_tools + google_analytics_tools + search_console_tools  # + admob_tools
